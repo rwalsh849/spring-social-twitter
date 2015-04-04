c50bbc779210e2d8565c62f65bdc78ce3280ca29
Hudson Mendes <hudsonmendes@gmail.com>
2015-04-03 22:16:15 -0300

    Unused imports removed

--- spring-social-twitter/src/test/java/org/springframework/social/twitter/api/impl/TwitterApiUriBuilderTest.java
+++ spring-social-twitter/src/test/java/org/springframework/social/twitter/api/impl/TwitterApiUriBuilderTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 import java.net.URI;
 import java.util.Arrays;
