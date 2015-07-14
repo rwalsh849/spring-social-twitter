a189fe7d5e90e23ce05ed02fb106ccf6674cedfc
Richard Walsh <richard@boxfish.com>
2015-07-14 11:10:36 -0700

    Fixing documentation / Enum error

--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/advertising/CampaignSorting.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/advertising/CampaignSorting.java
@@ -16,12 +16,16 @@
 package org.springframework.social.twitter.api.advertising;
 
 /**
- * All sorting options for querying {@link CardQuery}.
- * @author Richard Walsh
+ * All sorting options for querying {@link CampaignQuery}.
+ * @author Hudson Mendes
  */
 public enum CampaignSorting {
-	created_at,
-	updated_at,
-	deleted,
-	name
+    created_at,
+    updated_at,
+    deleted, name,
+    start_time,
+    end_time,
+    daily_budget_amount_local_micro,
+    total_budget_amount_local_micro,
+    standard_delivery
 }
--- spring-social-twitter/src/main/java/org/springframework/social/twitter/api/advertising/CardSorting.java
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/api/advertising/CardSorting.java
@@ -16,16 +16,11 @@
 package org.springframework.social.twitter.api.advertising;
 
 /**
- * All sorting options for querying {@link CampaignQuery}.
- * @author Hudson Mendes
+ * All sorting options for querying {@link CardQuery}.
+ * @author Richard Walsh
  */
 public enum CardSorting {
 	created_at,
 	updated_at,
-	deleted, name,
-	start_time,
-	end_time,
-	daily_budget_amount_local_micro,
-	total_budget_amount_local_micro,
-	standard_delivery
+	deleted, name
 }
